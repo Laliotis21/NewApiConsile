9d2d37990a68ae5517bc7ba9a719bc268d34ca24
Laliotis Nikos <Laliotisnikos4@gmail.com>
2022-02-20 00:54:25 +0200

    Changes to Console

--- src/main/java/NewsApiConsole.java
+++ src/main/java/NewsApiConsole.java
@@ -1,43 +1,52 @@
+
 import java.util.List;
 import java.util.Scanner;
 
 import exception.NewsApiException;
+import model.IpFind;
 import model.NewsAPi;
 import model.NewsInfo;
 import services.NewApiService;
 
+import model.NewsCategory;
+
 public class NewsApiConsole {
 
-	public static void main(String[] args) {{
+	public static void main(String[] args) throws NewsApiException {{
 		final NewApiService newsDBService = NewsAPi.getnewsapidb();
-
+		//final IpFind ipFind = newsDBService.getlocationData();
 		System.out.println("Welcome the the NewsApp");
 
 		while (true) {
 			System.out.println("Select one from the following options: ");
-			System.out.println("1. Show top rated movies for specified year.");
-			System.out.println("2. Search for movie.");
+			System.out.println("1. Get HeadLines.");
+			System.out.println("2. Search for nesw.");
 			System.out.println("3. Exit.");
 			System.out.println("Your choice: ");
 
+			@SuppressWarnings("resource")
 			Scanner sc = new Scanner(System.in);
 			String input = sc.nextLine();
 
-			System.out.println("Your choice: " + input);
 
 			switch (input) {
 			case "1":
-				System.out.println("Selected 1");
-				System.out.println("Enter country gb, gr : ");
-				String searchParam = sc.nextLine();
-				System.out.println("Enter Category from sports , business : ");
+				System.out.println("Enter Category from:");
+				System.out.println(java.util.Arrays.asList(NewsCategory.values()));
 				String searchParam2 = sc.nextLine();
+				
+				
 				try {
-									
+					
+					String results2;
+					results2 = newsDBService.getlocationData().getCountryCode();	
 					List<NewsInfo> results;
-					results = newsDBService.searchFortopHeadlines(searchParam, searchParam2);
+					results = newsDBService.searchFortopHeadlines(results2, searchParam2);
 					System.out.println("Results are: ");
 					System.out.println(results);
+					System.out.println();
+					
+					
 				} catch (NewsApiException e) {
 					System.err.println(e.getMessage());
 				}
